d094e1145fb34ab7726ed2c318c135ca54c57171
user name <mdisspt-s2266011@cirrus-login2.ib0.icexa.epcc.ed.ac.uk>
2025-03-16 02:56:43 +0000

    profiled with nsight

--- training_coursework/run_coursework.sh
+++ training_coursework/run_coursework.sh
@@ -11,6 +11,8 @@
 module load nvidia/cudnn/8.6.0-cuda-11.8
 module load python/3.10.8-gpu
 module load libsndfile/1.0.28
+module load nvidia/nvhpc 
+
 export PYTHONPATH=$PYTHONPATH:/work/y07/shared/cirrus-software/pytorch/1.13.1-gpu/python/3.10.8/lib/python3.10/site-packages
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work/y07/shared/cirrus-software/pytorch/1.13.1-gpu/python/3.10.8/lib
 export LIBRARY_PATH=$LIBRARY_PATH:/work/y07/shared/cirrus-software/pytorch/1.13.1-gpu/python/3.10.8/lib
@@ -20,5 +22,5 @@ export PYTHONUSERBASE=/work/mdisspt/mdisspt/$USER/python-installs
 export PYTHONPATH=/work/mdisspt/mdisspt/$USER/python-installs/lib/python3.10/site-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=/work/mdisspt/mdisspt/$USER/python-installs/lib/python3.10/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH
 
-srun -n 1 -c 10 python3 train.py --config short
+srun -n 1 -c 10 nsys profile -o transformer_profile python3 train.py --config short
 
